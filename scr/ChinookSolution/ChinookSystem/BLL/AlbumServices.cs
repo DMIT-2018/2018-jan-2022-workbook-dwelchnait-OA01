eb15534abac86432d61fa1fb3e1054afcf0460f6
Don Welch <dwelch@nait.ca>
2022-03-02 08:48:59 -0700

    Implement Paginator on Query page example

--- scr/ChinookSolution/ChinookSystem/BLL/AlbumServices.cs
+++ scr/ChinookSolution/ChinookSystem/BLL/AlbumServices.cs
@@ -26,9 +26,16 @@ namespace ChinookSystem.BLL
         #endregion
 
         #region Services: Queries
-        public List<AlbumsListBy> AlbumsByGenre(int genreid)
+        public List<AlbumsListBy> AlbumsByGenre(int genreid,
+                                                int pageNumber,
+                                                int pagesize,
+                                                out int totalrows)
         {
             //return raw data and let the presentation layer decide ordering
+            //pageNumbe, pageSize, totalrows are input parameters used for
+            //  paging on the presentation layer
+            //this query will return ONLY the rows that will be displayed on the
+            //  browser
             IEnumerable<AlbumsListBy> info = _context.Tracks
                                         .Where(x => x.GenreId == genreid
                                             &&  x.AlbumId.HasValue)
@@ -42,8 +49,20 @@ namespace ChinookSystem.BLL
                                             ArtistName = x.Album.Artist.Name,
                                             TrackCount = 0
                                         })
-                                        .Distinct();
-            return info.ToList();
+                                        .Distinct()
+                                        .OrderBy(x => x.Title);
+
+            //determine the size of the whole query collection
+            totalrows = info.Count();
+
+            //calculate the number of rows to SKIP in the query collection
+            //this calculation depends on the page number and page size
+            //page 1: skip 0 rows; page 2: skip pagesize rows; page 3: skip 2*pagesize rows ...
+            int skipRows = (pageNumber - 1) * pagesize;
+            //On the return, use the .Skip() and .Take() Linq extensions to extract the
+            //  actually row data to return
+
+            return info.Skip(skipRows).Take(pagesize).ToList();
         }
 
         #endregion
--- scr/ChinookSolution/WebApp/Pages/SamplePages/AlbumsByGenreQuery.cshtml.cs
+++ scr/ChinookSolution/WebApp/Pages/SamplePages/AlbumsByGenreQuery.cshtml.cs
@@ -55,8 +55,7 @@ namespace WebApp.Pages.SamplePages
         private const int PAGE_SIZE = 5;
         //instance for Paginator
         public Paginator Pager { get; set; }
-        //total rows in the complete query collection
-        public int TotalRows {get;set;}
+        
         #endregion
 
         //currentPage will appear on your Url as a Get Parameter value
@@ -77,13 +76,29 @@ namespace WebApp.Pages.SamplePages
                 //installing the paginator
                 //determine the page number to use on the paginator
                 int pageNumber = currentPage.HasValue ? currentPage.Value : 1;
+
                 //use the paginator to setup data needed for paging
                 PageState current = new(pageNumber, PAGE_SIZE);
+
+                //total rows in the complete query collection
+                int totalrows = 0;
+
                 //the RedirectToPage() causes a OnGet to execute
                 //obtain your raw data for your query
 
+                //for efficiency of data being transferred, we will pass the 
+                //  current page number and the desired page size to the backend query
+                //the returned collection will only have the rows of the whole query
+                //  collection that will actually be shown (PAGE-SIZE or less)
+                //the total number of records for the whole query collection will be
+                //  returned as an out parameter. This value is need by the Paginator
+                //  to set up its display
+                AlbumsOfGenre = _albumServices.AlbumsByGenre((int)GenreId,
+                                            pageNumber, PAGE_SIZE, out totalrows);
 
-                AlbumsOfGenre = _albumServices.AlbumsByGenre((int)GenreId);
+                //once the query is complete, use the returned row count in instantizating
+                //  an instance of the Paginator
+                Pager = new Paginator(totalrows, current);
             }
         }
 
