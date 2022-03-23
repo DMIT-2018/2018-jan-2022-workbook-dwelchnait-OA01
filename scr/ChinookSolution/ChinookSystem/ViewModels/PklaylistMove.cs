5c5621c1d0d1575285ae09a93ea3e8ce330b75bf
Don Welch <dwelch@nait.ca>
2022-03-23 08:33:01 -0600

    Complete Data Models

---
+++ scr/ChinookSolution/ChinookSystem/ViewModels/PklaylistMove.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChinookSystem.ViewModels
+{
+    public class PklaylistMove
+    {
+        public int TrackId { get; set; }
+        public bool SelectedTrack { get; set; }
+        public string TrackInput { get; set; }  
+    }
+}
